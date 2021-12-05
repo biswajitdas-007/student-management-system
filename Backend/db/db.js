35da5ccfc562a30ff12322c05c1e4a9db6ecc2a3
Biswajit Das <biswajitdas7894@gmail.com>
2021-12-05 22:52:29 +0530

    student database management

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
-
+require("dotenv").config();
 const connect = () => {
-    return mongoose.connect("mongodb+srv://biswajit:6SKpjnaWfEB04Ihr@cluster0.mzxys.mongodb.net/biswajit007?retryWrites=true&w=majority")
+    return mongoose.connect(`mongodb+srv://biswajit:${process.env.KEY}@cluster0.mzxys.mongodb.net/biswajit007?retryWrites=true&w=majority`)
 }
 
 module.exports = connect;
